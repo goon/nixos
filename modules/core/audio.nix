510a368baffbdb33aac3d078e2841fd92dbe9c0a
goon <hayhurst@protonmail.com>
2026-03-29 03:21:53 +0100

    feat: added & configured mpd

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -23,4 +23,25 @@
   environment.systemPackages = with pkgs; [
     pulseaudio
   ];
+
+  # MPD Music Player Daemon
+  services.mpd = {
+    enable = true;
+    user = "michael";
+    settings = {
+      music_directory = "/home/michael/Music";
+      audio_output = [
+        {
+          type = "pulse";
+          name = "PipeWire Output";
+        }
+      ];
+    };
+  };
+
+  # MPD service needs access to user's PipeWire session
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/1000";
+    PULSE_SERVER = "unix:/run/user/1000/pulse/native";
+  };
 }
