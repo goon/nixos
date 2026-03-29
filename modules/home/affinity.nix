ba34a57309c117cd689f01c65a77015e8452e4f3
goon <hayhurst@protonmail.com>
2026-03-29 23:42:56 +0100

    feat: affinity.nix

--- modules/home/affinity.nix
+++ modules/home/affinity.nix
@@ -1,12 +1,6 @@
 { pkgs, inputs, user, ... }:
 
 {
-  # Garnix binary cache for affinity-nix
-  nix.settings = {
-    substituters = [ "https://cache.garnix.io" ];
-    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJPXNJQ=" ];
-  };
-
   home-manager.users.${user} = {
     home.packages = [
       inputs.affinity-nix.packages.${pkgs.stdenv.system}.v3
