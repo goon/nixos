dd8fa3b2ccb18ed4fa04ef897d310eb3a75ea041
goon <hayhurst@protonmail.com>
2026-03-31 21:00:59 +0100

    fix: comments

--- modules/core/peripherals.nix
+++ modules/core/peripherals.nix
@@ -3,10 +3,11 @@
 {
   # Logitech wireless devices
   hardware.logitech.wireless.enable = true;
-  boot.kernelModules = [ "hid-logitech-hidpp" ];
+  boot.kernelModules = [ "hid-logitech-hidpp" ]; # For non-G Logitech devices
 
   # Wooting keyboard support
   services.udev.packages = [ pkgs.wooting-udev-rules ];
+
   environment.systemPackages = with pkgs; [
     wootility
     brightnessctl
