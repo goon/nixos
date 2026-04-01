f6472f3d4f3ba31a8f860fa70863da5128b6c9f6
goon <hayhurst@protonmail.com>
2026-04-02 00:50:01 +0100

    fix: removed redundant mkOptions

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -1,114 +1,84 @@
 {
-  pkgs,
-  lib,
   config,
   ...
 }:
 
 {
-  options.xdgDefaults = with lib; {
-    browser = mkOption {
-      type = types.str;
-      default = "firefox.desktop";
-      description = "Default browser desktop entry";
-    };
-
-    imageViewerPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
-    imageViewer = mkOption {
-      type = types.str;
-      default = "org.gnome.Loupe.desktop";
-    };
-
-    mediaPlayerPackage = mkOption {
-      type = types.nullOr types.package;
-      default = null;
-    };
-    mediaPlayer = mkOption {
-      type = types.str;
-      default = "org.gnome.Totem.desktop";
-    };
-
-    fileManager = mkOption {
-      type = types.str;
-      default = "org.gnome.Nautilus.desktop";
-    };
-
-    documentViewer = mkOption {
-      type = types.str;
-      default = "org.gnome.Evince.desktop";
-    };
-
-    musicPlayer = mkOption {
-      type = types.str;
-      default = "org.gnome.Totem.desktop";
-    };
-  };
-
   config =
     let
-      cfg = config.xdgDefaults;
+      inherit (config.globals) apps;
     in
     {
-      home.packages =
-        (lib.optional (cfg.imageViewerPackage != null) cfg.imageViewerPackage)
-        ++ (lib.optional (cfg.mediaPlayerPackage != null) cfg.mediaPlayerPackage);
+      home.packages = [ ]; # Packages are managed in gnome.nix or individual modules
 
       xdg.mimeApps = {
         enable = true;
         defaultApplications = {
           # ----- Browser
-          "x-scheme-handler/http" = cfg.browser;
-          "x-scheme-handler/https" = cfg.browser;
-          "text/html" = cfg.browser;
-          "image/svg+xml" = cfg.browser;
+          "x-scheme-handler/http" = apps.browser;
+          "x-scheme-handler/https" = apps.browser;
+          "text/html" = apps.browser;
+          "image/svg+xml" = apps.browser;
 
           # ----- Images
-          "image/jpeg" = cfg.imageViewer;
-          "image/png" = cfg.imageViewer;
-          "image/gif" = cfg.imageViewer;
-          "image/webp" = cfg.imageViewer;
-          "image/bmp" = cfg.imageViewer;
-          "image/tiff" = cfg.imageViewer;
+          "image/jpeg" = apps.imageViewer;
+          "image/png" = apps.imageViewer;
+          "image/gif" = apps.imageViewer;
+          "image/webp" = apps.imageViewer;
+          "image/bmp" = apps.imageViewer;
+          "image/tiff" = apps.imageViewer;
 
           # ----- Video
-          "video/mp4" = cfg.mediaPlayer;
-          "video/webm" = cfg.mediaPlayer;
-          "video/x-matroska" = cfg.mediaPlayer;
-          "video/x-msvideo" = cfg.mediaPlayer;
-          "video/quicktime" = cfg.mediaPlayer;
-          "video/x-mpeg" = cfg.mediaPlayer;
+          "video/mp4" = apps.videoPlayer;
+          "video/webm" = apps.videoPlayer;
+          "video/x-matroska" = apps.videoPlayer;
+          "video/x-msvideo" = apps.videoPlayer;
+          "video/quicktime" = apps.videoPlayer;
+          "video/x-mpeg" = apps.videoPlayer;
 
           # ----- Audio
-          "audio/mpeg" = cfg.musicPlayer;
-          "audio/flac" = cfg.musicPlayer;
-          "audio/ogg" = cfg.musicPlayer;
-          "audio/wav" = cfg.musicPlayer;
-          "audio/aac" = cfg.musicPlayer;
-          "audio/x-vorbis+ogg" = cfg.musicPlayer;
-          "audio/x-flac" = cfg.musicPlayer;
-          "audio/x-wav" = cfg.musicPlayer;
+          "audio/mpeg" = apps.musicPlayer;
+          "audio/flac" = apps.musicPlayer;
+          "audio/ogg" = apps.musicPlayer;
+          "audio/wav" = apps.musicPlayer;
+          "audio/aac" = apps.musicPlayer;
+          "audio/x-vorbis+ogg" = apps.musicPlayer;
+          "audio/x-flac" = apps.musicPlayer;
+          "audio/x-wav" = apps.musicPlayer;
 
           # ----- File Manager
-          "inode/directory" = cfg.fileManager;
+          "inode/directory" = apps.fileManager;
 
           # ----- Documents
-          "application/pdf" = cfg.documentViewer;
-          "application/epub+zip" = cfg.documentViewer;
-          "application/x-mobipocket-ebook" = cfg.documentViewer;
-          "application/vnd.comicbook-rar" = cfg.documentViewer;
-          "application/vnd.comicbook+zip" = cfg.documentViewer;
+          "application/pdf" = apps.pdfViewer;
+          "application/epub+zip" = apps.pdfViewer;
+          "application/x-mobipocket-ebook" = apps.pdfViewer;
+          "application/vnd.comicbook-rar" = apps.pdfViewer;
+          "application/vnd.comicbook+zip" = apps.pdfViewer;
+
+          # ----- Text & Config
+          "text/plain" = apps.editor;
+          "text/markdown" = apps.editor;
+          "application/toml" = apps.editor;
+          "text/x-toml" = apps.editor;
+          "text/x-config" = apps.editor;
+          "text/x-yaml" = apps.editor;
+          "application/yaml" = apps.editor;
+          "application/x-yaml" = apps.editor;
+          "text/x-python" = apps.editor;
+          "text/x-shellscript" = apps.editor;
+          "text/x-makefile" = apps.editor;
+          "text/x-csrc" = apps.editor;
+          "text/x-c++src" = apps.editor;
         };
       };
 
-      # ----- Override → Neovim Wrapper 
+      # ----- Override → Neovim Wrapper
       xdg.desktopEntries.nvim = {
         name = "Neovim";
         genericName = "Text Editor";
-        exec = "nvim %F";
-        terminal = true;
+        exec = "${config.globals.userTerminal} nvim %F";
+        terminal = false;
         type = "Application";
         categories = [
           "Utility"
@@ -132,6 +102,14 @@
           "application/x-shellscript"
           "text/x-c"
           "text/x-c++"
+          "application/toml"
+          "text/x-toml"
+          "text/x-config"
+          "text/x-yaml"
+          "application/yaml"
+          "application/x-yaml"
+          "text/markdown"
+          "text/x-python"
         ];
       };
     };
