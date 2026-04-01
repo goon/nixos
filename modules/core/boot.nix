02b4921b6536bc4a8afedb626c16868551f0e60d
goon <hayhurst@protonmail.com>
2026-04-01 03:19:14 +0100

    feat: switched to zen kernel

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,12 +1,11 @@
-_:
-
+{ pkgs, ... }:
 {
-  # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
 
-  # Quiet boot - suppress kernel/initrd messages
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
   boot.kernelParams = [
