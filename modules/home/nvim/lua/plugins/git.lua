480d1eb21d09f51d72d1ac0a258420b6d626ab67
goon <hayhurst@protonmail.com>
2026-04-01 01:44:12 +0100

    removal: replaced by .nix configurations

--- modules/home/nvim/lua/plugins/git.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Gitsigns for git integration
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {}, -- Default setup
-  },
-}
--- modules/home/yazi/init.lua
+++
@@ -1,6 +0,0 @@
-require ("git"):setup {
-  order = 1500,
-}
-
--- Remove permissions from status bar
-Status:children_remove(4, Status.RIGHT)
