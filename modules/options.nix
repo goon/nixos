8f6657c80d14a45a7576eb9757e055f3fbc9b726
goon <hayhurst@protonmail.com>
2026-04-02 00:49:38 +0100

    feat: separated _module.args into mkOptions

---
+++ modules/options.nix
@@ -0,0 +1,78 @@
+{ lib, ... }:
+
+let
+  inherit (lib) mkOption types;
+in
+{
+  # ========== structural essentials ==========
+  # We use _module.args for these to avoid infinite recursion
+  # when they are used in attribute names (like users.users.${username})
+  config = {
+    _module.args = {
+      username = "michael";
+      repoName = ".nixos";
+    };
+  };
+
+  # ========== system settings ==========
+  # We use the "Pro" mkOption pattern for these for better discovery
+  options.globals = {
+    userTerminal = mkOption {
+      type = types.str;
+      default = "kitty";
+      description = "default terminal emulator";
+    };
+
+    userFonts = {
+      sansSerif = mkOption {
+        type = types.str;
+        default = "Outfit";
+        description = "global sans-serif font";
+      };
+      monospace = mkOption {
+        type = types.str;
+        default = "Kode Mono";
+        description = "global monospace font";
+      };
+    };
+
+    # ========== Default Applications ==========
+    apps = {
+      browser = mkOption {
+        type = types.str;
+        default = "firefox.desktop";
+        description = "default web browser";
+      };
+      editor = mkOption {
+        type = types.str;
+        default = "nvim.desktop";
+        description = "default text editor";
+      };
+      imageViewer = mkOption {
+        type = types.str;
+        default = "org.gnome.Loupe.desktop";
+        description = "default image viewer";
+      };
+      videoPlayer = mkOption {
+        type = types.str;
+        default = "org.gnome.Totem.desktop";
+        description = "default video player";
+      };
+      musicPlayer = mkOption {
+        type = types.str;
+        default = "org.gnome.Totem.desktop";
+        description = "default music player";
+      };
+      fileManager = mkOption {
+        type = types.str;
+        default = "org.gnome.Nautilus.desktop";
+        description = "default file manager";
+      };
+      pdfViewer = mkOption {
+        type = types.str;
+        default = "org.gnome.Evince.desktop";
+        description = "default document viewer";
+      };
+    };
+  };
+}
