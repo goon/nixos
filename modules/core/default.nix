0830faa67f74dbf2d78d2e064d6d14414cbd5499
goon <hayhurst@protonmail.com>
2026-04-01 22:19:49 +0100

    fix: added imports

---
+++ modules/core/default.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  # ========== Core System Imports ==========
+  imports = [
+    ./boot.nix
+    ./filesystems.nix
+    ./hardware.nix
+    ./locale.nix
+    ./networking.nix
+    ./nix.nix
+    ./power.nix
+    ./security.nix
+    ./user.nix
+  ];
+}
