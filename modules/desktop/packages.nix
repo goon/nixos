48b3788c03fd49ac68bb320434794174fdeb0dda
goon <hayhurst@protonmail.com>
2026-04-01 22:20:30 +0100

    chore: package list cleanup

--- modules/desktop/packages.nix
+++ modules/desktop/packages.nix
@@ -7,12 +7,8 @@
 
     fd # File Finder
     ripgrep # Improved Grep
-    zellij # Multiplexer
-    btop # Resource Monitor
-    cava # Audio Visualizer
     wget # Web Downloads
     curl # Curl
-    rmpc # Music Player
 
     # ------- Development Tools
 
@@ -29,7 +25,6 @@
     gowall # Wallpaper Themer
     antigravity # Google IDE
     google-chrome # Google Web Browser
-    pear-desktop # Youtube Music Wrapper (Electron)
     nicotine-plus # GUI client for Soulseek Network
 
     # ------- Theme Tools
