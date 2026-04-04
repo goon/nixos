52fc9d4d718c02ad38e0201d51d8ea460ad2d309
goon <hayhurst@protonmail.com>
2026-04-04 23:42:43 +0100

    init

---
+++ modules/extra/nixcord.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  options.module.nixcord.enable = lib.mkEnableOption "Nixcord (Declarative Discord/Vesktop)" // {
+    default = true;
+  };
+
+  config = lib.mkIf config.module.nixcord.enable {
+    home-manager.users.${config._module.args.username} = {
+      imports = [ inputs.nixcord.homeModules.nixcord ];
+
+      programs.nixcord = {
+        enable = true;
+        vesktop.enable = true;
+        config = {
+          useQuickCss = true;
+          transparent = true;
+          themeLinks = [ ];
+          enabledThemes = [ "qsTheme.css" ];
+          plugins = {
+            alwaysAnimate.enable = true;
+            imageZoom.enable = true;
+          };
+        };
+      };
+
+    };
+  };
+}
