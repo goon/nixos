af75b55c4d88b871a03a35905e51a4265268369f
goon <hayhurst@protonmail.com>
2026-04-04 23:43:14 +0100

    feat: backup

--- modules/home.nix
+++ modules/home.nix
@@ -3,6 +3,7 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
 
   home-manager.users.${username} = {
     home.username = username;
