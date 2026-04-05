5bec6450a45e6b41633c4dc0774f51ef4c96995e
goon <hayhurst@protonmail.com>
2026-04-05 16:15:12 +0100

    feat: wayland tools extracted from packages.nix

---
+++ modules/session/wayland.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options.module.wayland.enable = lib.mkEnableOption "Wayland Utilities (cliphist, wl-clipboard, etc.)" // {
+    default = true;
+  };
+
+  config = lib.mkIf config.module.wayland.enable {
+    home-manager.users.${config._module.args.username} = {
+      home.packages = with pkgs; [
+        cliphist
+        wl-clipboard
+        libnotify
+      ];
+    };
+  };
+}
