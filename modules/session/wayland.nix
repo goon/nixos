499f207a2d9a8ec370190ab0c99a8d48f4420244
goon <hayhurst@protonmail.com>
2026-04-06 02:33:31 +0100

    fix: formatting

--- modules/session/wayland.nix
+++ modules/session/wayland.nix
@@ -6,9 +6,11 @@
 }:
 
 {
-  options.module.wayland.enable = lib.mkEnableOption "Wayland Utilities (cliphist, wl-clipboard, etc.)" // {
-    default = true;
-  };
+  options.module.wayland.enable =
+    lib.mkEnableOption "Wayland Utilities (cliphist, wl-clipboard, etc.)"
+    // {
+      default = true;
+    };
 
   config = lib.mkIf config.module.wayland.enable {
     home-manager.users.${config._module.args.username} = {
