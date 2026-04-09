00a7b4cf38826a285ee015539b0c66d87f3bf89d
goon <hayhurst@protonmail.com>
2026-04-09 16:42:56 +0100

    chore: removed ignore

--- modules/lib/recursive.nix
+++ modules/lib/recursive.nix
@@ -30,7 +30,7 @@ let
             len > 4 && builtins.substring (len - 4) 4 name == ".nix" && name != "default.nix" && !isPrivate;
         in
         if type == "directory" then
-          if name == "lib" || name == "scripts" || isPrivate then
+          if name == "lib" || isPrivate then
             [ ] # Skip forbidden directories
           else if builtins.pathExists (fullPath + "/default.nix") then
             [ (fullPath + "/default.nix") ]
