44638a28cf1d2cbc8fc4b859259685af9c7f1963
goon <hayhurst@protonmail.com>
2026-04-11 17:15:15 +0100

    init

---
+++ modules/extra/proton.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.module.proton;
+in
+{
+  options.module.proton.enable = lib.mkEnableOption "Proton Applications" // {
+    default = true;
+  };
+
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${config._module.args.username} = {
+      home.packages = [
+        pkgs.protonmail-desktop
+        pkgs.proton-pass
+      ];
+    };
+  };
+}
