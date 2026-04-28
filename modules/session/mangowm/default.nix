f5e55da7382225861d6162f753e788255d7a26e4
goon <hayhurst@protonmail.com>
2026-04-28 04:18:45 +0100

    feat: mangowm added as a compositor option

---
+++ modules/session/mangowm/default.nix
@@ -0,0 +1,34 @@
+{
+  inputs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  imports = [
+    inputs.mangowm.nixosModules.mango
+  ];
+
+  config = lib.mkIf (config.module.desktop.windowmanager == "mangowm") {
+    # Use the official NixOS module for system-level setup
+    programs.mango.enable = true;
+
+    home-manager.users.${config._module.args.username} =
+      { config, osConfig, pkgs, ... }:
+      {
+        imports = [
+          inputs.mangowm.hmModules.mango
+        ];
+
+        home.packages = [ pkgs.lswt ];
+
+        # Enable the HM module
+        wayland.windowManager.mango.enable = true;
+
+        # Maintain the repo's pattern of out-of-store symlinks for session configs
+        xdg.configFile."mango".source =
+          config.lib.file.mkOutOfStoreSymlink "${osConfig.globals.repoPath}/modules/session/mangowm";
+      };
+  };
+}
