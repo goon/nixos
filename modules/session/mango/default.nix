cae9cc8f1a17e44f614615b993bf4180c0d8a4e6
goon <hayhurst@protonmail.com>
2026-05-24 20:26:54 +0100

    feat: pkg extraction

--- modules/session/mango/default.nix
+++ modules/session/mango/default.nix
@@ -28,28 +28,12 @@
         home.packages = with pkgs; [
           lswt
           wlrctl
-          grim
-          slurp
-          swappy
         ];
 
         wayland.windowManager.mango.enable = true;
 
         xdg.configFile."mango".source =
           config.lib.file.mkOutOfStoreSymlink "${osConfig.globals.repoPath}/modules/session/mango";
-
-        xdg.configFile."swappy/config".text = ''
-          [Default]
-          save_dir=${osConfig.globals.paths.home}/Pictures/Screenshots
-          save_filename_format=swappy-%Y%m%d-%H%M%S.png
-          show_panel=false
-          line_size=5
-          text_size=20
-          text_font=sans-serif
-          paint_mode=brush
-          early_exit=true
-          fill_shape=false
-        '';
       };
   };
 }
