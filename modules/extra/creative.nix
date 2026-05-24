b5d5ce60bacae433a0cc927e81328cb173a8b125
goon <hayhurst@protonmail.com>
2026-05-24 17:39:08 +0100

    feat: hidden desktop entries

--- modules/extra/creative.nix
+++ modules/extra/creative.nix
@@ -19,6 +19,33 @@
         pkgs.affinity-v3
         pkgs.davinci-resolve
       ];
+
+      home.file = {
+        ".local/share/applications/blackmagicraw-player.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Name=Blackmagic RAW Player
+          NoDisplay=true
+        '';
+        ".local/share/applications/blackmagicraw-speedtest.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Name=Blackmagic RAW Speed Test
+          NoDisplay=true
+        '';
+        ".local/share/applications/davinci-control-panels-setup.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Name=DaVinci Control Panels Setup
+          NoDisplay=true
+        '';
+        ".local/share/applications/davinci-fairlight-studio-utility.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Name=Fairlight Studio Utility
+          NoDisplay=true
+        '';
+      };
     };
 
     # Hardware tweaks for DaVinci Resolve on Radeon
