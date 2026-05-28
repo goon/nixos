777e949727198039d3a39bcc6e70a514f086d18f
goon <hayhurst@protonmail.com>
2026-05-28 07:29:13 +0100

    feat: vscodium

--- modules/extra/dev.nix
+++ modules/extra/dev.nix
@@ -18,6 +18,25 @@
           python3
           go
         ];
+
+        programs.vscodium = {
+          enable = true;
+          profiles.default = {
+            extensions = with pkgs.vscode-extensions; [
+              jnoortheen.nix-ide
+              pkief.material-icon-theme
+            ];
+            userSettings = {
+              "window.titleBarStyle" = "custom";
+              "window.customTitleBarVisibility" = "never";
+              "window.menuBarVisibility" = "hidden";
+              "editor.fontFamily" = "'${config.globals.userFonts.monospace}', 'monospace'";
+              "workbench.statusBar.visible" = false;
+              "editor.minimap.enabled" = false;
+              "workbench.iconTheme" = "material-icon-theme";
+            };
+          };
+        };
       }
     ];
   };
