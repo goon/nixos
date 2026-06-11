b2df839b61ed22f0f4e9d97f3018639f2216625a
goon <hayhurst@protonmail.com>
2026-06-11 13:58:59 +0100

    feat: vpn

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,7 +1,9 @@
-_:
+{ pkgs, ... }:
 
 {
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
   hardware.bluetooth.enable = true;
+
+  environment.systemPackages = [ pkgs.proton-vpn ];
 }
