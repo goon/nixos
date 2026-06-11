a7fbddb74798ca2499a11ad0cbf65acf73d97059
goon <hayhurst@protonmail.com>
2026-06-11 16:38:32 +0100

    feat: global pkgs/zed removed & vscodium expanded

--- modules/extra/dev.nix
+++ modules/extra/dev.nix
@@ -13,29 +13,39 @@
   config = lib.mkIf config.module.dev.enable {
     home-manager.sharedModules = [
       {
-        home.packages = with pkgs; [
-          nodejs
-          python3
-          go
-          zed-editor
-        ];
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
 
         programs.vscodium = {
           enable = true;
           profiles.default = {
-            extensions = with pkgs.vscode-extensions; [
-              jnoortheen.nix-ide
-              pkief.material-icon-theme
-            ];
-            userSettings = {
-              "window.titleBarStyle" = "custom";
-              "window.customTitleBarVisibility" = "never";
-              "window.menuBarVisibility" = "hidden";
-              "editor.fontFamily" = "'${config.globals.userFonts.monospace}', 'monospace'";
-              "workbench.statusBar.visible" = false;
-              "editor.minimap.enabled" = false;
-              "workbench.iconTheme" = "material-icon-theme";
-            };
+            extensions =
+              with pkgs.vscode-extensions;
+              [
+                jnoortheen.nix-ide
+              ]
+              ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                  name = "qt-core";
+                  publisher = "TheQtCompany";
+                  version = "1.15.0";
+                  sha256 = "0iip8hl9yplv1jq0167xyg0jzvfk4j0qqyxjrjk2fbr8jv3xymqr";
+                }
+                {
+                  name = "qt-qml";
+                  publisher = "TheQtCompany";
+                  version = "1.15.0";
+                  sha256 = "1nnx5w03clg3qml9kqm89pp5na20907ra413g99ilqcfdshjf8jj";
+                }
+                {
+                  name = "opencode";
+                  publisher = "sst-dev";
+                  version = "0.0.13";
+                  sha256 = "1m301j2qbym3j2qnck76jyxakca3h1qiybc2r7wy7z11m98mg9z9";
+                }
+              ];
           };
         };
       }
