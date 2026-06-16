41dd4055d5d171aae159d62f8079771c0bae0602
goon <hayhurst@protonmail.com>
2026-06-16 17:15:57 +0100

    feat: garnix abandoned > cachix

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -25,10 +25,10 @@ lib.module config "nix" true {
         auto-optimise-store = true;
         warn-dirty = false;
         substituters = [
-          "https://cache.garnix.io"
+          "https://nix-community.cachix.org"
         ];
         trusted-public-keys = [
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
       };
 
