837af583c2ab331d9c841a1fbafd4ab6ef229b58
goon <hayhurst@protonmail.com>
2026-06-16 19:51:17 +0100

    feat: removed

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,8 +3,6 @@
 {
   networking.networkmanager = {
     enable = true;
-    dns = "systemd-resolved";
-    wifi.powersave = true;
   };
   services.resolved.enable = true;
   networking.firewall.enable = true;
