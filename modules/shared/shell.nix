6e161712609618501fc15bf3e74866ef9bf421c8
goon <hayhurst@protonmail.com>
2026-06-20 01:35:55 +0100

    refactor: clean up shell module

--- modules/shared/shell.nix
+++ modules/shared/shell.nix
@@ -24,77 +24,76 @@ lib.module config "shell" true {
 
   userPkgs = with pkgs; [
     cheat-cmd
-    duf
-    fd
-    gum
-    ripgrep
-    wget
     curl
+    duf
+    gum
     unzip
-    btop
-    jq
+    wget
   ];
 
-  home = { config, ... }: {
+  home = { config, osConfig, ... }: {
     programs = {
-      eza.enable = true;
       bat.enable = true;
+      btop.enable = true;
+      eza.enable = true;
+      fd.enable = true;
       fzf.enable = true;
+      jq.enable = true;
+      ripgrep.enable = true;
+
+      zoxide = {
+        enable = true;
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+        options = [
+          "--cmd cd"
+        ];
+      };
+
+      bash = {
+        enable = true;
+        enableCompletion = true;
+        historyControl = [
+          "ignoreboth"
+          "erasedups"
+        ];
+        historyFileSize = 20000;
+        shellOptions = [
+          "histappend"
+          "checkwinsize"
+        ];
+      };
+
+      zsh = {
+        enable = true;
+        dotDir = "${config.xdg.configHome}/zsh";
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+      };
     };
 
     home = {
       shellAliases = {
         ".." = "cd ..";
         "..." = "cd ../..";
-        ls = "eza --icons --git";
-
         cat = "bat";
         df = "duf";
-        tree = "eza --tree";
+        f = "fzf";
         find = "fd";
         grep = "rg";
+        ls = "eza --icons --git";
         rm = "rm -i";
-        f = "fzf";
+        tree = "eza --tree";
       };
 
       sessionVariables = {
-        BROWSER = "firefox";
+        BROWSER = lib.removeSuffix ".desktop" osConfig.globals.apps.browser;
       };
 
       sessionPath = [
         "$HOME/.local/bin"
       ];
-    };
-
-    programs.zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      options = [
-        "--cmd cd"
-      ];
-    };
-
-    programs.bash = {
-      enable = true;
-      enableCompletion = true;
-      historyControl = [
-        "ignoreboth"
-        "erasedups"
-      ];
-      historyFileSize = 20000;
-      shellOptions = [
-        "histappend"
-        "checkwinsize"
-      ];
-    };
-
-    programs.zsh = {
-      enable = true;
-      dotDir = "${config.xdg.configHome}/zsh";
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
     };
   };
 }
