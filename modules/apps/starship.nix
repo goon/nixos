47ca65a96d5f82cef66bf0a3e0b92f2c28c37f22
goon <hayhurst@protonmail.com>
2026-06-27 08:35:21 +0100

    fix: line break

--- modules/apps/starship.nix
+++ modules/apps/starship.nix
@@ -10,7 +10,8 @@ lib.module config "starship" false {
       enable = true;
       settings = {
         "$schema" = "https://starship.rs/config-schema.json";
-        format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+        format = "$username$hostname$directory$git_branch$cmd_duration$line_break$character";
+        add_newline = false;
         directory.style = "blue";
         character = {
           success_symbol = "[❯](purple)";
@@ -21,30 +22,9 @@ lib.module config "starship" false {
           format = "[$branch]($style)";
           style = "bright-black";
         };
-        git_status = {
-          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-          style = "cyan";
-          conflicted = "​";
-          untracked = "​";
-          modified = "​";
-          staged = "​";
-          renamed = "​";
-          deleted = "​";
-          stashed = "≡";
-        };
-        git_state = {
-          format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
-          style = "bright-black";
-        };
         cmd_duration = {
           format = "[$duration]($style) ";
           style = "yellow";
-        };
-        python = {
-          format = "[$virtualenv]($style) ";
-          style = "bright-black";
-          detect_extensions = [ ];
-          detect_files = [ ];
         };
       };
     };
