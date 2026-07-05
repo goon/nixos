b6c287604031a63ff8efca9cf1371097947abbd7
goon <hayhurst@protonmail.com>
2026-07-05 13:25:40 +0100

    style: comments removed

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -12,14 +12,12 @@ lib.module config "gaming" false {
 
   nixpkgs.overlays = [ inputs.millennium.overlays.default ];
 
-  # ========== System Packages ==========
   environment.systemPackages = with pkgs; [
     mangohud
     bolt-launcher
     runelite
   ];
 
-  # ========== Programs & Services ==========
   programs = {
     steam = {
       enable = true;
@@ -34,17 +32,14 @@ lib.module config "gaming" false {
     gamemode.enable = true;
   };
 
-  # ========== Environment & Hardware tweaks ==========
   environment.variables = {
     SDL_VIDEODRIVER = lib.mkForce "wayland,x11,windows";
   };
 
-  # Kernel performance (vm.max_map_count)
   boot.kernel.sysctl = {
     "vm.max_map_count" = 2147483642;
   };
 
-  # ========== Binary Caches ==========
   nix.settings = {
     substituters = [ "https://nix-gaming.cachix.org" ];
     trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
