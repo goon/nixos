accc80bc8361bdfb063878776117ad5aa3e422b0
goon <hayhurst@protonmail.com>
2026-07-05 13:25:54 +0100

    feat: active border colour to native hm module

--- modules/apps/kitty.nix
+++ modules/apps/kitty.nix
@@ -24,6 +24,7 @@ lib.module config "kitty" false {
         tab_bar_margin_height = "20 20";
         tab_bar_style = "powerline";
         tab_powerline_style = "round";
+        active_border_color = "none";
       };
 
       keybindings = {
@@ -37,7 +38,6 @@ lib.module config "kitty" false {
 
       extraConfig = ''
         include ~/.cache/quickshell/themes/kitty.conf
-        active_border_color none
         symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3eb,U+e5fa-U+e6b1,U+e700-U+e7c5,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f532,U+f0001-U+f1af0 Symbols Nerd Font Mono
       '';
     };
