6d3bcbeb1340e24570ea0abca6612c228ac419f8
goon <hayhurst@protonmail.com>
2026-08-23 16:52:09 +0100

    chore: cleanup

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -6,47 +6,49 @@
   ...
 }:
 lib.module config "gaming" false {
-  imports = [
-    inputs.nix-gaming.nixosModules.pipewireLowLatency
-  ];
+  config = {
+    imports = [
+      inputs.nix-gaming.nixosModules.pipewireLowLatency
+    ];
 
-  nixpkgs.overlays = [ inputs.millennium.overlays.default ];
+    nixpkgs.overlays = [ inputs.millennium.overlays.default ];
+
+    environment.systemPackages = with pkgs; [
+      mangohud
+      bolt-launcher
+      runelite
+      deadlock-mod-manager
+    ];
+
+    programs = {
+      steam = {
+        enable = true;
+        package = pkgs.millennium-steam;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        extraCompatPackages = [ pkgs.proton-ge-bin ];
+      };
+
+      gamescope.enable = true;
+      gamemode.enable = true;
+    };
+
+    environment.variables = {
+      SDL_VIDEODRIVER = lib.mkForce "wayland,x11,windows";
+    };
+
+    boot.kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+
+    nix.settings = {
+      substituters = [ "https://nix-gaming.cachix.org" ];
+      trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+    };
+  };
 
   homeManager = _: {
     home.packages = [ (pkgs.callPackage ../../pkgs/jagex.nix { }) ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    mangohud
-    bolt-launcher
-    runelite
-    deadlock-mod-manager
-  ];
-
-  programs = {
-    steam = {
-      enable = true;
-      package = pkgs.millennium-steam;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraCompatPackages = [ pkgs.proton-ge-bin ];
-    };
-
-    gamescope.enable = true;
-    gamemode.enable = true;
-  };
-
-  environment.variables = {
-    SDL_VIDEODRIVER = lib.mkForce "wayland,x11,windows";
-  };
-
-  boot.kernel.sysctl = {
-    "vm.max_map_count" = 2147483642;
-  };
-
-  nix.settings = {
-    substituters = [ "https://nix-gaming.cachix.org" ];
-    trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
   };
 }
