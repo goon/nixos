fb3ef9c28fdc403b0429886347874e346b5b6472
goon <hayhurst@protonmail.com>
2026-09-25 17:49:25 +0100

    feat: -vsc

--- modules/apps/dev.nix
+++ modules/apps/dev.nix
@@ -14,42 +14,5 @@ lib.module config "dev" false {
       enable = true;
       nix-direnv.enable = true;
     };
-
-    programs.vscodium = {
-      enable = true;
-      profiles.default = {
-        extensions =
-          with pkgs.vscode-extensions;
-          [
-            jnoortheen.nix-ide
-          ]
-          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-            {
-              name = "qt-core";
-              publisher = "TheQtCompany";
-              version = "1.15.0";
-              sha256 = "0iip8hl9yplv1jq0167xyg0jzvfk4j0qqyxjrjk2fbr8jv3xymqr";
-            }
-            {
-              name = "qt-qml";
-              publisher = "TheQtCompany";
-              version = "1.15.0";
-              sha256 = "1nnx5w03clg3qml9kqm89pp5na20907ra413g99ilqcfdshjf8jj";
-            }
-            {
-              name = "opencode";
-              publisher = "sst-dev";
-              version = "0.0.13";
-              sha256 = "1m301j2qbym3j2qnck76jyxakca3h1qiybc2r7wy7z11m98mg9z9";
-            }
-            {
-              name = "symbols";
-              publisher = "miguelsolorio";
-              version = "0.0.26";
-              sha256 = "52b9059b0c2a314b02bb064ddbfb7cb9e0d0059d561334c5f48aa2ae64c06595";
-            }
-          ];
-      };
-    };
   };
 }
